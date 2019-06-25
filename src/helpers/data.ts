4ab6294fae3b282e531109b449209d995d5caa52
jingchuan <3161141062@qq.com>
2019-06-25 14:48:07 +0800

    feat: support post request data parse

---
+++ src/helpers/data.ts
@@ -0,0 +1,21 @@
+import { isPlainObject } from './util'
+
+// 将object对象转换为JSON格式
+export function transformRequest(data: any): any {
+  if (isPlainObject(data)) {
+    return JSON.stringify(data)
+  }
+  return data
+}
+
+// 将后端返回的json格式转换为Object
+export function transformReponse(data: any): any {
+  if (typeof data === 'string') {
+    try {
+      data = JSON.parse(data)
+    } catch (e) {
+      // TODO
+    }
+  }
+  return data
+}
