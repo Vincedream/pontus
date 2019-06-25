780dc60c22fbb2377ce284fc4b7c929101675b0d
jingchuan <3161141062@qq.com>
2019-06-25 14:47:24 +0800

    feat: support url parse

---
+++ src/helpers/url.ts
@@ -0,0 +1,58 @@
+import { isDate, isObject } from './util'
+
+function encode(val: string): string {
+  return encodeURIComponent(val)
+    .replace(/%40/g, '@')
+    .replace(/%3A/gi, ':')
+    .replace(/%24/g, '$')
+    .replace(/%2C/gi, ',')
+    .replace(/%20/g, '+')
+    .replace(/%5B/gi, '[')
+    .replace(/%5D/gi, ']')
+}
+
+// 将params上的参数转换为string格式，拼接到url上
+export function buildURL(url: string, params?: any): string {
+  if (!params) {
+    return url
+  }
+
+  const parts: string[] = []
+
+  Object.keys(params).forEach(key => {
+    const val = params[key]
+    if (val === null || typeof val === undefined) {
+      return
+    }
+    let values = []
+    if (Array.isArray(val)) {
+      values = val
+      key += '[]'
+    } else {
+      values = [val]
+    }
+    values.forEach(val => {
+      if (isDate(val)) {
+        // 处理Date类型
+        val = val.toISOString()
+      } else if (isObject(val)) {
+        // 处理Object
+        val = JSON.stringify(val)
+      }
+      parts.push(`${encode(key)}=${encode(val)}`)
+    })
+  })
+
+  let serializedParams = parts.join('&')
+
+  if (serializedParams) {
+    // 去除hash
+    const markIndex = url.indexOf('#')
+    if (markIndex !== -1) {
+      url = url.slice(0, markIndex)
+    }
+    url += (url.indexOf('?') === -1 ? '?' : '&') + serializedParams
+  }
+
+  return url
+}
