553dd30d2d3f3dbba7521df27829622b5e383471
jingchuan <3161141062@qq.com>
2019-07-01 21:00:28 +0800

    example: update

--- examples/config/app.ts
+++ examples/config/app.ts
@@ -1,39 +1,39 @@
-import pontus from '../../src/index'
+import pontus, { PontusTransformer } from '../../src/index'
 import qs from 'qs'
 
-pontus.defaults.headers.common['test2'] = 123
+// pontus.defaults.headers.common['test2'] = 123
 
-pontus({
-  url: '/config/post',
-  method: 'post',
-  data: qs.stringify({
-    a: 1
-  }),
-  headers: {
-    test: '321'
-  }
-}).then((res) => {
-  console.log(res.data)
-})
-
-// axios({
-//   transformRequest: [(function(data) {
-//     return qs.stringify(data)
-//   }), ...(axios.defaults.transformRequest as AxiosTransformer[])],
-//   transformResponse: [...(axios.defaults.transformResponse as AxiosTransformer[]), function(data) {
-//     if (typeof data === 'object') {
-//       data.b = 2
-//     }
-//     return data
-//   }],
+// pontus({
 //   url: '/config/post',
 //   method: 'post',
-//   data: {
+//   data: qs.stringify({
 //     a: 1
+//   }),
+//   headers: {
+//     test: '321'
 //   }
 // }).then((res) => {
 //   console.log(res.data)
 // })
+
+pontus({
+  transformRequest: [(function(data) {
+    return qs.stringify(data)
+  }), ...(pontus.defaults.transformRequest as PontusTransformer[])],
+  transformResponse: [...(pontus.defaults.transformResponse as PontusTransformer[]), function(data) {
+    if (typeof data === 'object') {
+      data.b = 2
+    }
+    return data
+  }],
+  url: '/config/post',
+  method: 'post',
+  data: {
+    a: 1
+  }
+}).then((res) => {
+  console.log(res.data)
+})
 
 // const instance = axios.create({
 //   transformRequest: [(function(data) {
