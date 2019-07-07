b293d2a3985b23539f7f31f2b307fe306f5e5362
jingchuan <3161141062@qq.com>
2019-07-07 22:39:43 +0800

    example: update

--- examples/more/app.ts
+++ examples/more/app.ts
@@ -1,9 +1,9 @@
-import axios, { AxiosError } from '../../src/index'
+import pontus, { PontusError } from '../../src/index'
 import qs from 'qs'
 //
-// import 'nprogress/nprogress.css'
+import 'nprogress/nprogress.css'
 //
-// import NProgress from 'nprogress'
+import NProgress from 'nprogress'
 
 // document.cookie = 'a=b'
 // //
@@ -17,20 +17,20 @@ import qs from 'qs'
 //   console.log(res)
 // })
 //
-const instance = axios.create({
-  xsrfCookieName: 'XSRF-TOKEN-D',
-  xsrfHeaderName: 'X-XSRF-TOKEN-D'
-})
+// const instance = axios.create({
+//   xsrfCookieName: 'XSRF-TOKEN-D',
+//   xsrfHeaderName: 'X-XSRF-TOKEN-D'
+// })
 
-instance.get('/more/get').then(res => {
-  console.log(res)
-})
+// instance.get('/more/get').then(res => {
+//   console.log(res)
+// })
 // const instance = axios.create()
-//
+// //
 // function calculatePercentage(loaded: number, total: number) {
 //   return Math.floor(loaded * 1.0) / total
 // }
-//
+
 // function loadProgressBar() {
 //   const setupStartProgress = () => {
 //     instance.interceptors.request.use(config => {
@@ -38,7 +38,7 @@ instance.get('/more/get').then(res => {
 //       return config
 //     })
 //   }
-//
+
 //   const setupUpdateProgress = () => {
 //     const update = (e: ProgressEvent) => {
 //       console.log(e)
@@ -47,7 +47,7 @@ instance.get('/more/get').then(res => {
 //     instance.defaults.onDownloadProgress = update
 //     instance.defaults.onUploadProgress = update
 //   }
-//
+
 //   const setupStopProgress = () => {
 //     instance.interceptors.response.use(response => {
 //       NProgress.done()
@@ -57,28 +57,28 @@ instance.get('/more/get').then(res => {
 //       return Promise.reject(error)
 //     })
 //   }
-//
+
 //   setupStartProgress()
 //   setupUpdateProgress()
 //   setupStopProgress()
 // }
-//
+
 // loadProgressBar()
-//
+
 // const downloadEl = document.getElementById('download')
-//
+
 // downloadEl!.addEventListener('click', e => {
 //   instance.get('https://img.mukewang.com/5cc01a7b0001a33718720632.jpg')
 // })
-//
+
 // const uploadEl = document.getElementById('upload')
-//
+
 // uploadEl!.addEventListener('click', e => {
 //   const data = new FormData()
 //   const fileEl = document.getElementById('file') as HTMLInputElement
 //   if (fileEl.files) {
 //     data.append('file', fileEl.files[0])
-//
+
 //     instance.post('/more/upload', data)
 //   }
 // })
@@ -100,7 +100,7 @@ instance.get('/more/get').then(res => {
 // }).catch((e: AxiosError) => {
 //   console.log(e.message)
 // })
-//
+
 // axios.get('/more/304', {
 //   validateStatus(status) {
 //     return status >= 200 && status < 400
@@ -110,13 +110,13 @@ instance.get('/more/get').then(res => {
 // }).catch((e: AxiosError) => {
 //   console.log(e.message)
 // })
-//
+
 // axios.get('/more/get', {
 //   params: new URLSearchParams('a=b&c=d')
 // }).then(res => {
 //   console.log(res)
 // })
-//
+
 // axios.get('/more/get', {
 //   params: {
 //     a: 1,
@@ -126,13 +126,13 @@ instance.get('/more/get').then(res => {
 // }).then(res => {
 //   console.log(res)
 // })
-//
+
 // const instance = axios.create({
 //   paramsSerializer(params) {
 //     return qs.stringify(params, { arrayFormat: 'brackets' })
 //   }
 // })
-//
+
 // instance.get('/more/get', {
 //   params: {
 //     a: 1,
@@ -146,38 +146,38 @@ instance.get('/more/get').then(res => {
 // const instance = axios.create({
 //   baseURL: 'https://img.mukewang.com/'
 // })
-//
+
 // instance.get('5cc01a7b0001a33718720632.jpg')
-//
+
 // instance.get('https://img.mukewang.com/szimg/5becd5ad0001b89306000338-360-202.jpg')
 
-// function getA() {
-//   return axios.get('/more/A')
-// }
+function getA() {
+  return pontus.get('/more/A')
+}
 
-// function getB() {
-//   return axios.get('/more/B')
-// }
+function getB() {
+  return pontus.get('/more/B')
+}
 
-// axios.all([getA(), getB()])
-//   .then(axios.spread(function(resA, resB) {
-//     console.log(resA.data)
-//     console.log(resB.data)
-//   }))
+pontus.all([getA(), getB()])
+  .then(pontus.spread(function(resA, resB) {
+    console.log(resA.data)
+    console.log(resB.data)
+  }))
 
-// axios.all([getA(), getB()])
-//   .then(([resA, resB]) => {
-//     console.log(resA.data)
-//     console.log(resB.data)
-//   })
+pontus.all([getA(), getB()])
+  .then(([resA, resB]) => {
+    console.log(resA.data)
+    console.log(resB.data)
+  })
 
-// const fakeConfig = {
-//   baseURL: 'https://www.baidu.com/',
-//   url: '/user/12345',
-//   params: {
-//     idClient: 1,
-//     idTest: 2,
-//     testString: 'thisIsATest'
-//   }
-// }
-// console.log(axios.getUri(fakeConfig))
+const fakeConfig = {
+  baseURL: 'https://www.baidu.com/',
+  url: '/user/12345',
+  params: {
+    idClient: 1,
+    idTest: 2,
+    testString: 'thisIsATest'
+  }
+}
+console.log(pontus.getUri(fakeConfig))
--- examples/server.js
+++ examples/server.js
@@ -1,10 +1,13 @@
 const express = require('express')
-const bodyParser = require('body-parser')
-const webpack = require('webpack')
 const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const multipart = require('connect-multiparty')
+const webpack = require('webpack')
+const atob = require('atob')
 const webpackDevMiddleware = require('webpack-dev-middleware')
 const webpackHotMiddleware = require('webpack-hot-middleware')
 const WebpackConfig = require('./webpack.config')
+const path = require('path')
 
 require('./server2')
 
@@ -31,6 +34,10 @@ app.use(bodyParser.json())
 // app.use(bodyParser.text())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
+
+app.use(multipart({
+  uploadDir: path.resolve(__dirname, 'upload-file')
+}))
 
 const router = express.Router()
 
@@ -181,5 +188,36 @@ function registerCancelRouter() {
 function registerMoreRouter() {
   router.get('/more/get', function(req, res) {
     res.json(req.cookies)
+  })
+
+  router.post('/more/upload', function(req, res) {
+    console.log(req.body, req.files)
+    res.end('upload success')
+  })
+
+  router.post('/more/post', function(req, res) {
+    const auth = req.headers.authorization
+    const [type, credentials] = auth.split(' ')
+    console.log(atob(credentials))
+    const [username, password] = atob(credentials).split(':')
+    if (type === 'Basic' && username === 'Yee' && password === '123456') {
+      res.json(req.body)
+    } else {
+      res.status(401)
+      res.end('UnAuthorization')
+    }
+  })
+
+  router.get('/more/304', function(req, res) {
+    res.status(304)
+    res.end()
+  })
+
+  router.get('/more/A', function(req, res) {
+    res.end('A')
+  })
+
+  router.get('/more/B', function(req, res) {
+    res.end('B')
   })
 }
